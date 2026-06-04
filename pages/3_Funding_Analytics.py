9496f90cb2f94ee24b70989aabc5721211140452
Rani <ranirani76233@gmail.com>
2026-06-04 11:34:52 +0530

    Update 3_Funding_Analytics.py

--- pages/3_Funding_Analytics.py
+++ pages/3_Funding_Analytics.py
@@ -1,1 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("💰 Funding Analytics")
+
+fig = px.box(
+    df,
+    x="Industry",
+    y="Funding Amount (M USD)",
+    color="Industry"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+corr = df.select_dtypes(
+    include="number"
+).corr()
+
+fig2 = px.imshow(
+    corr,
+    text_auto=True,
+    title="Correlation Heatmap"
+)
+
+st.plotly_chart(
+    fig2,
+    use_container_width=True
+)
