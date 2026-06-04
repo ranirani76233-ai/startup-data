33f30d7f0913826f3f43687e9fe86c1699be0b6e
Rani <ranirani76233@gmail.com>
2026-06-04 11:36:01 +0530

    Update 4_AI_Insights.py

--- pages/4_AI_Insights.py
+++ pages/4_AI_Insights.py
@@ -1,1 +1,39 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("🤖 AI Business Insights")
+
+best_industry = (
+    df.groupby("Industry")
+    ["Valuation (M USD)"]
+    .mean()
+    .idxmax()
+)
+
+best_region = (
+    df.groupby("Region")
+    ["Funding Amount (M USD)"]
+    .sum()
+    .idxmax()
+)
+
+highest_revenue = (
+    df.loc[
+        df["Revenue (M USD)"].idxmax(),
+        "Startup Name"
+    ]
+)
+
+st.success(
+    f"🏆 Highest Valuation Industry: {best_industry}"
+)
+
+st.info(
+    f"🌍 Most Funded Region: {best_region}"
+)
+
+st.warning(
+    f"💰 Highest Revenue Startup: {highest_revenue}"
+)
