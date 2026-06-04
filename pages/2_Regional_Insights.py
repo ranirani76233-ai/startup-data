94113a94e068411efb205ac8fc1cfc6b2e05a79d
Rani <ranirani76233@gmail.com>
2026-06-04 11:33:44 +0530

    Update 2_Regional_Insights.py

--- pages/2_Regional_Insights.py
+++ pages/2_Regional_Insights.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("🌎 Regional Insights")
+
+region_count = (
+    df.groupby("Region")
+    .size()
+    .reset_index(name="Count")
+)
+
+fig = px.pie(
+    region_count,
+    names="Region",
+    values="Count",
+    hole=0.5
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
